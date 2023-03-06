105e12ddd90662d6f8eba9e07a6b214f80b482f8
Sudip Sarkar <sudipsarkar419@gmail.com>
2023-03-06 14:18:23 +0530

    mongo atlas connect

--- config/key.js
+++ config/key.js
@@ -1,8 +1,18 @@
+// const mongoose=require('mongoose');
+
+// mongoose.connect('mongodb://127.0.0.1/Myhabit-tracker');
+// const db=mongoose.connection;
+// db.on('error',console.log.bind(console,"Error Connecting"));
+// db.once('open',()=>{
+//     console.log("Connecting to Database:MongoDB");
+// })
+
 const mongoose=require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1/Myhabit-tracker');
+mongoose.connect('mongodb+srv://sudip98:6VL7uaRkMtUN7XV4@cluster0.f9bjwrh.mongodb.net/?retryWrites=true&w=majority');
 const db=mongoose.connection;
 db.on('error',console.log.bind(console,"Error Connecting"));
 db.once('open',()=>{
     console.log("Connecting to Database:MongoDB");
 })
+
