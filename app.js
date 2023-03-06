e21449cac39f276f53244baaba6aa5e442e0afde
Sudip Sarkar <sudipsarkar419@gmail.com>
2023-03-06 11:36:38 +0530

    port mody

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const expressLayouts = require('express-ejs-layouts');
-const port=8001;
+const port=process.env.PORT ||  8001;
 const mongoose=require('mongoose');
 const session=require('express-session');
 const flash=require('connect-flash');
